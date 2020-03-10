157287db75d5ef2e097c14e1d8985816dffeca4e
y.kuzmich <evgen.kuzmich@gmail.com>
2020-03-10 23:24:35 +0300

    Minimal working configuration

--- src/test/java/by/jenka/surefire/ClassTest.java
+++ src/test/java/by/jenka/surefire/ClassTest.java
@@ -1,11 +1,22 @@
 package by.jenka.surefire;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 public class ClassTest {
+
+    @DisplayName("TEST true is true")
     @Test
     public void test() {
-        Assertions.assertTrue(true);
+        System.out.println("PRINT in TEST 1");
+        Assertions.assertTrue(false, "This is true");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("PRINT in TEST 2");
+        Assertions.assertFalse(false, "This is false");
     }
 }
