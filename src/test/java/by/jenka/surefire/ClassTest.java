f72b0ad32f32ed5e5d29e4124bfac771939f1d7d
y.kuzmich <evgen.kuzmich@gmail.com>
2020-03-10 22:54:48 +0300

    initial commit

---
+++ src/test/java/by/jenka/surefire/ClassTest.java
@@ -0,0 +1,11 @@
+package by.jenka.surefire;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ClassTest {
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
